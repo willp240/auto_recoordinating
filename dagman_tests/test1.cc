a7c10742586c091c1212ac049df0e2e19d124e69
parkerw <william.parker@physics.ox.ac.uk>
2023-08-04 23:28:57 +0100

    adding some silly basic execs for testing

---
+++ dagman_tests/test1.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int argc, char **argv ){
+
+  std::cout << "test 1 doing " << std::endl;
+
+  return 0;
+}
---
+++ dagman_tests/test2.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int argc, char **argv ){
+
+  std::cout << "test 2 doing" << std::endl;
+
+  return 0;
+}
