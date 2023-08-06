dca658207cd470777d23ffa4524966f0af1f534f
parkerw <william.parker@physics.ox.ac.uk>
2023-08-06 19:33:14 +0100

    starting some skeleton pseudo code of full process

---
+++ dagman_tests/pseduo_sev.sh
@@ -0,0 +1,12 @@
+
+
+### run the simulation 
+### will need to give it output names etc
+
+### when finished
+### read in simulation and reconstruct with different velocities
+
+
+### when all finished use the tools to get best fit velocity
+
+### now use that velocity to make multiPDF pdf using tools 
